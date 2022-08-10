78b5dd40e02f7a5e40c321b3c440d4d03eb4a443
Matheus Fonseca <teus.fonseca@hotmail.com>
2022-08-10 15:45:01 -0300

    all project

---
+++ index.js
@@ -0,0 +1,75 @@
+import Controls from "./controls.js";
+import Timer from "./timer.js";
+import Sound from "./sounds.js";
+import {
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop,
+  buttonSoundOff,
+  buttonSoundOn,
+  minutesDisplay,
+  secondsDisplay,
+} from "./elements.js";
+
+const sound = Sound();
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop,
+});
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset,
+});
+
+///////////////////////////////
+
+buttonPlay.addEventListener("click", function () {
+  controls.play();
+  timer.countdown();
+  sound.pressButton();
+});
+
+buttonPause.addEventListener("click", function () {
+  controls.pause();
+  timer.hold();
+  sound.pressButton();
+});
+
+buttonStop.addEventListener("click", function () {
+  controls.reset();
+  timer.reset();
+  sound.pressButton();
+});
+
+/////////////////SOUND//////////////////
+
+buttonSoundOn.addEventListener("click", function () {
+  buttonSoundOn.classList.toggle("hide");
+  buttonSoundOff.classList.toggle("hide");
+  sound.bgAudio.play();
+});
+buttonSoundOff.addEventListener("click", function () {
+  buttonSoundOn.classList.toggle("hide");
+  buttonSoundOff.classList.toggle("hide");
+  sound.bgAudio.pause();
+});
+
+//////////////////SOUND /////////////////
+
+buttonSet.addEventListener("click", function () {
+  let newMinutes = controls.getMinutes();
+
+  if (!newMinutes) {
+    timer.reset();
+    return;
+  }
+
+  timer.updateDisplay(newMinutes, 0);
+  timer.updateMinutes(newMinutes);
+});
---
+++ sounds.js
@@ -0,0 +1,22 @@
+export default function () {
+  const audio = document.querySelector("audio");
+  const buttonPressAudio = new Audio(
+    "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true"
+  );
+  const kitchenTimer = new Audio(
+    "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true"
+  );
+  const bgAudio = audio;
+
+  bgAudio.loop;
+
+  function pressButton() {
+    buttonPressAudio.play();
+  }
+
+  function timeEnd() {
+    kitchenTimer.play();
+  }
+
+  return { pressButton, timeEnd, bgAudio };
+}
